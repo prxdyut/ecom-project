38517b0a58940af277bdc1e13e6177ee25bafd52
Pradyuts-Program <77787607+Pradyuts-Program@users.noreply.github.com>
2022-08-14 11:45:26 +0530

    v1.1

--- pages/Login/index.js
+++ pages/Login/index.js
@@ -76,7 +76,6 @@ export default function LoginPage() {
                     marginLeft: "auto",
                   }}
                 >
-                  {" "}
                   Don't have an account? <Link href={"/Signup"}> Signup </Link>
                 </Typography>
                 </Stack>
